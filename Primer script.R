0881ef0ee8b9c290b27b60c5e4fc3dee3f421a81
ronnyhdez <ronny.hernandezm@gmail.com>
2017-03-27 20:51:26 -0600

    Primera etapa implementación github en trabajo de talleres R-luminatis

---
+++ Primer script.R
@@ -0,0 +1,16 @@
+
+#Material creado para el taller de R-luminatis.
+#Por Ronny Hdez y Álvaro Vega.
+#Si usted quiere ser partícipe de los talleres, 
+#o quiere organizar este mismo taller en su centro de estudio o grupo de trabajo, 
+#puede contactarnos a los correos:
+
+# > Contacto: ronny.hernandez@gmail.com / alvarovh95@gmail.com
+
+#------------------Inicio de sesión-----------------------------------------------------
+
+getwd()
+
+
+#-------------------------Importar datos----------------------------------------
+
