bc0a2a8664f0679f68a7e49a723452f200e0a6dc
Álvaro Vega-Hidalgo <alvarovegahd@gmail.com>
2017-06-27 15:46:47 -0600

    Apuntes rapidos

--- Primer script.R
+++ Primer script.R
@@ -14,3 +14,26 @@ getwd()
 
 #-------------------------Importar datos----------------------------------------
 
+#Mae, de vez en cuando voy a agregar cosas a lo random y luego un dia lo arreglamos tuanis
+
+La parte de la justificación de por qué usarlos. empezar con lo bsico, resaltar la importancia de la medida de efecto,
+el poder que agrega y luego mandarnos con lo demás:
+
+where ε is normally distributed. This book presents three extensions to this framework.
+The first generalizes the y part; the second, the ε part; and the third, the x part of the linear
+model.
+Generalized Linear Models: The standard linear model cannot handle nonnormal
+responses, y, such as counts or proportions. This motivates the development of
+generalized linear models that can represent categorical, binary and other response types.
+
+Mixed Effect Models: Some data has a grouped, nested or hierarchical structure.
+Repeated measures, longitudinal and multilevel data consist of several observations taken
+on the same individual or group. This induces a correlation structure in the error, ε.
+Mixed effect models allow the modeling of such data.
+
+Nonparametric Regression Models: In the linear model, the predictors, x, are
+combined in a linear way to model the effect on the response. Sometimes this linearity is
+insufficient to capture the structure of the data and more flexibility is required. Methods
+such as additive models, trees and neural networks allow a more flexible regression
+modeling of the response that combine the predictors in a nonparametric manner. 
+
